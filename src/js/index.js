809d0e40ac4a88ed9cc1ca5c4f302b1d55efc893
nvquyet2224 <nvquyet2224@gmail.com>
2025-05-10 18:29:33 +0700

    update
    - basic

---
+++ src/js/index.js
@@ -0,0 +1,198 @@
+import "../sass/index.scss";
+import $ from "jquery";
+import Swiper, { Pagination, Navigation, Scrollbar, Mousewheel, FreeMode } from "swiper";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { CountUp } from 'countup.js';
+
+
+export { Swiper, Pagination };
+export { $ };
+
+function motionGallery() {
+  if (document.querySelector(".motionSlider")) {
+    new Swiper(".motionSlider", {
+      modules: [Navigation],
+      effect: "slide",
+      loop: false,
+      speed: 800,
+      preloadImages: false,
+      lazy: true,
+      slidesPerView: 4,
+      spaceBetween: 4,
+      freeMode: true,
+      grabCursor: true,
+      allowTouchMove: true,
+      watchOverflow: true,
+      // scrollbar: {
+      //   el: '.luminous__gallery .swiper-scrollbar',
+      //   hide: false,
+      //   draggable: true
+      // },
+      // mousewheel: {
+      //   sensitivity: 1,
+      //   forceToAxis: true, // Bắt buộc cuộn theo trục chính
+      //   releaseOnEdges: true, // Tiếp tục cuộn khi đến cuối
+      // },
+      breakpoints: {
+        1023: {
+          slidesPerView: 4,
+          spaceBetween: 4
+        }
+      },
+      // pagination: {
+      //   el: ".luminous .swiper-pagination",
+      //   clickable: true,
+      // },
+      navigation: {
+        nextEl: "#motion .swiper-button-next",
+        prevEl: "#motion .swiper-button-prev",
+      },
+      on: {
+        init: function (swiper) { },
+        transitionStart: function () {
+        },
+        transitionEnd: function () { },
+      },
+    });
+  }
+
+}
+
+
+function collectionGallery() {
+  if (document.querySelector(".collectionSlider")) {
+    new Swiper(".collectionSlider", {
+      modules: [Navigation],
+      effect: "slide",
+      loop: false,
+      speed: 800,
+      preloadImages: false,
+      lazy: true,
+      slidesPerView: 3,
+      spaceBetween: 4,
+      freeMode: true,
+      grabCursor: true,
+      allowTouchMove: true,
+      watchOverflow: true,
+      // scrollbar: {
+      //   el: '.luminous__gallery .swiper-scrollbar',
+      //   hide: false,
+      //   draggable: true
+      // },
+      // mousewheel: {
+      //   sensitivity: 1,
+      //   forceToAxis: true, // Bắt buộc cuộn theo trục chính
+      //   releaseOnEdges: true, // Tiếp tục cuộn khi đến cuối
+      // },
+      breakpoints: {
+        1023: {
+          slidesPerView: 3,
+          spaceBetween: 4
+        }
+      },
+      // pagination: {
+      //   el: ".luminous .swiper-pagination",
+      //   clickable: true,
+      // },
+      navigation: {
+        nextEl: "#collection .swiper-button-next",
+        prevEl: "#collection .swiper-button-prev",
+      },
+      on: {
+        init: function (swiper) { },
+        transitionStart: function () {
+        },
+        transitionEnd: function () { },
+      },
+    });
+  }
+
+}
+
+
+function onScroll() {
+  var windowH = 0;
+  setTimeout(() => {
+    windowH = $(window).innerHeight();
+    $('.page').attr({ 'style': `--height:${windowH}px` });
+  }, 350);
+
+  // Function to add/remove class on scroll
+  var lastScrollTop = 0;
+  function toggleClassOnView() {
+    setTimeout(() => {
+      const items = `.infrastructure__building--floor,
+        .infrastructure__building--light,
+        .infrastructure__box .title,
+        .infrastructure__building--main,
+        .infrastructure__information,
+        .infrastructure .box-center,
+        .infrastructure__information--list,
+        .infrastructure__special`;
+      $(items).each(function () {
+        if ($(this).length) {
+          var elementTop = $(this).offset().top;
+          var elementBottom = elementTop + $(this).outerHeight();
+          var viewportTop = $(window).scrollTop();
+          var viewportBottom = viewportTop + $(window).height();
+
+          // Kiá»ƒm tra náº¿u pháº§n tá»­ Ä‘Ã£ vÃ o khung nhÃ¬n
+          if (elementTop < viewportBottom && elementBottom > viewportTop) {
+            $(this).addClass('in-view');
+            if ($('.infrastructure__information--list.in-view')) {
+
+              if ($('#number1').length) {
+                number1.update(+$('#number1').attr('data-end'));
+              }
+              if ($('#number2').length) {
+                number2.update(+$('#number2').attr('data-end'));
+              }
+              if ($('#number3').length) {
+                number3.update(+$('#number3').attr('data-end'));
+              }
+
+            }
+          }
+
+          // Kiá»ƒm tra náº¿u pháº§n tá»­ Ä‘Ã£ thoÃ¡t khá»i khung nhÃ¬n
+          if (elementBottom <= viewportTop || elementTop >= viewportBottom) {
+            $(this).removeClass('in-view');
+          }
+
+        }
+
+      });
+      var scrollTop = $(this).scrollTop();
+      if (scrollTop > lastScrollTop) {
+        $('.header').removeClass('sticky');
+      } else if (scrollTop < lastScrollTop) {
+        if (scrollTop <= $('.banner__box').height()) {
+          $('.header').removeClass('sticky');
+        } else {
+          $('.header').addClass('sticky');
+        }
+
+      }
+      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+    }, 50);
+  }
+
+  //$(window).on('scroll resize', toggleClassOnView);
+  $(window).on('scroll', toggleClassOnView);
+  $(window).on('resize', function () {
+    toggleClassOnView();
+    setTimeout(() => {
+      windowH = $(window).innerHeight();
+      $('.page').attr({ 'style': `--height:${windowH}px` });
+    }, 10);
+  });
+
+  toggleClassOnView();
+
+}
+
+(function () {
+  motionGallery();
+  collectionGallery();
+})();
---
+++ webpack.config.js
@@ -0,0 +1,137 @@
+const path = require("path");
+//const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const CopyPlugin = require("copy-webpack-plugin");
+
+let fs = require("fs");
+
+const preload = fs.readFileSync(__dirname + "/src/_preload.html");
+const headerComponent = fs.readFileSync(__dirname + "/src/_component-header.html");
+const footerComponent = fs.readFileSync(__dirname + "/src/_component-footer.html");
+const svg = fs.readFileSync(__dirname + "/src/_svg.html");
+
+
+//const page404 = fs.readFileSync(__dirname + "/src/_page-404.html");
+
+const minifyRules = {
+  collapseWhitespace: true,
+  removeComments: true,
+  removeRedundantAttributes: false,
+  removeScriptTypeAttributes: true,
+  removeStyleLinkTypeAttributes: true, 
+};
+
+const devMode = true;
+const isBack = false;
+const copyScript = true; // false -- > min
+const minify = false;
+
+module.exports = {
+  mode: devMode ? "development" : "production",
+  watch: true,
+  entry: {
+    reload: ["./src/js/reload.js"],
+    index: ["./src/js/index.js"]
+  },
+  output: {
+    publicPath: "../",
+    path: path.resolve(__dirname, "html"),
+    filename: isBack ? "../../source/plugins/Web/webroot/js/[name].js" : "js/[name].js", // copy to backend
+    library: ["[name]", "modules"],
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "fonts/[name].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: isBack ? "../../source/plugins/Web/webroot/css/[name].css" : "css/[name].css", // copy to backend
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/images", to: "images", noErrorOnMissing: true },
+        { from: "src/js/common.js",
+          to: isBack ? "../../source/plugins/Web/webroot/js/common.js" : "js/common.js",
+          info: { minimized: copyScript },
+          noErrorOnMissing: true,
+        },
+        { from: "src/videos", to: "videos", noErrorOnMissing: true }
+      ],
+      options: {
+        concurrency: 100
+      },
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      filename: "index.html",
+      preload: preload,
+      svg: svg,
+      header: headerComponent,
+      footer: footerComponent,
+      template: "src/index.html",
+      minify: minify,
+    })
+  ],
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  watchOptions: {
+    ignored: "**/node_modules",
+    aggregateTimeout: 100,
+  },
+};
